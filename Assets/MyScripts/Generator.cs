e31ddc8c283a6168011ead123fc92934bbe867cf
Shigema <tsubakyun4869@gmail.com>
2023-10-04 10:08:59 +0900

    [add] 生成できるまでの時間をゲージで可視化・時間が未経過でもフラグを真にして予約するようなことをできないようにした

--- Assets/MyScripts/Generator.cs
+++ Assets/MyScripts/Generator.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 一定時間おきに指定したプレハブから GameObject を生成するコンポーネント
@@ -6,37 +7,53 @@ using UnityEngine;
 public class Generator : MonoBehaviour
 {
     /// <summary>一定時間おきに生成する GameObject の元となるプレハブ</summary>
-    [SerializeField] GameObject m_prefab = default;
+    [SerializeField] GameObject _prefab = default;
     /// <summary>生成する間隔（秒）</summary>
-    [SerializeField] float m_interval = 1f;
+    [SerializeField] float _interval = 1f;
     /// <summary>true の場合、開始時にまず生成する</summary>
-    [SerializeField] bool m_generateOnStart = true;
+    [SerializeField] bool _generateOnStart = true;
     /// <summary>タイマー計測用変数</summary>
-    float m_timer;
+    [SerializeField] float _timer;
     [SerializeField, Tooltip("ボタンがクリックされた")] bool _isClick;
-
     [SerializeField, Tooltip("スポーン場所")] GameObject _gameObject;
+    [SerializeField, Tooltip("クリックできるかUI")] Slider _slider;
+    [SerializeField, Tooltip("時間経過まえにフラグ真にするのを防ぐ")] Button _button;
 
     void Start()
     {
-        if (m_generateOnStart)
+
+        if (_slider)
         {
-            m_timer = m_interval;
+            _slider.maxValue = _interval;
+            _slider.value = 0;
+        }
+        _button.enabled = false;
+        if (_generateOnStart)
+        {
+            _timer = _interval;
+            _button.enabled = true;
         }
     }
 
     public void Update()
     {
         // Time.deltaTime は「前フレームからの経過時間」を取得する
-        m_timer += Time.deltaTime;
+        _timer += Time.deltaTime;
 
         // 「経過時間」が「生成する間隔」を超えたら
-        if (m_timer > m_interval && _isClick)
+        if (_timer > _interval)
         {
-            m_timer = 0;    // タイマーをリセットしている
-            Instantiate(m_prefab, _gameObject.transform.position, Quaternion.identity);
-            _isClick = false;
+            _button.enabled = true;
+            if (_isClick)
+            {
+                _timer = 0;    // タイマーをリセットしている
+                Instantiate(_prefab, _gameObject.transform.position, Quaternion.identity);
+                _isClick = false;
+                _button.enabled = false;
+            }
         }
+        _slider.value = _timer;
+
     }
 
     public void OnClick()
