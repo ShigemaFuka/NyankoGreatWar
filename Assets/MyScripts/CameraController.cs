20949178d1efbb3c6ba774a161d5b978e93a9060
Shigema <tsubakyun4869@gmail.com>
2023-10-07 15:50:53 +0900

    [fixed] カメラ移動のスクリプト

--- Assets/MyScripts/CameraController.cs
+++ Assets/MyScripts/CameraController.cs
@@ -7,17 +7,17 @@ using UnityEngine.UI;
 public class CameraController : MonoBehaviour
 {
     [Tooltip("x軸方向")] float _xValue;
-    GameObject _sliderObj;
-    Slider _slider;
+    //GameObject _sliderObj;
+    [SerializeField, Tooltip("画面を左右に移動するスライド")] Slider _slider;
 
     void Start()
     {
         // 位置 初期化
         this.transform.position = new Vector3(-15, 0, -10);
 
-        // Slider関係を参照
-        _sliderObj = GameObject.Find("Slider"); 
-        _slider = _sliderObj.GetComponent<Slider>();
+        //// Slider関係を参照
+        //_sliderObj = GameObject.Find("Slider"); 
+        //_slider = _sliderObj.GetComponent<Slider>();
 
         // MaxValueとMinValueを入れて、範囲を決める
         _slider.maxValue = 0;
